734c7c4b9c75c97efc3f89414938db1d7fc72827
Kyle Cook <kyle.cook@kibocommerce.com>
2020-04-28 13:18:54 -0400

    Added feature getting and creating

--- mysite/Feature/models.py
+++ mysite/Feature/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Feature(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(unique=True, max_length=100)
+    percent = models.IntegerField()
+    enabled = models.BooleanField()
--- mysite/Feature/views.py
+++ mysite/Feature/views.py
@@ -1,10 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.views.decorators.csrf import csrf_exempt
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+
+from .models import Feature
+
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
-    return HttpResponse('THIS IS A FEATURE')
+    if request.method == 'GET':
+        if request.GET.get('id', ''):
+            id = request.GET.get('id', '')
+            feature = Feature.objects.filter(id=id).values()
+            return HttpResponse(feature)
+        else:
+            features = Feature.objects.all().values()
+            return HttpResponse(features)
+    elif request.method == 'POST':
+        id = request.POST.get('id', '')
+        name = request.POST.get('name', '')
+        percent = request.POST.get('percent', '')
+        enabled = request.POST.get('enabled', '')
+        newFeature = Feature(id, name, percent, enabled)
+        newFeature.save()
+        return HttpResponse('Nice', status=200)
+    elif request.method == 'PATCH:':
+        
+        return HttpResponse('Nice', status=200)
+    elif request.method == 'DELETE':
+        return HttpResponse(request.POST)
